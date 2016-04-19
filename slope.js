e4b383381e43da768d79fd9e03a1b4f83dc0ad5d
superkun2010 <kuhsu@hotmail.com>
2016-04-19 15:33:32 -0700

    Added if statement

--- slope.js
+++ slope.js
@@ -1,8 +1,18 @@
-x1 = 10
-x2 = 12
+var x1 = 10;
+var x2 = 12;
 
-m = 1/2
-b = 0.3
+var m = 1/2;
+var b = 0.3;
 
-y1 = m * x1 + b
-y2 = m * x2 + b
+var y1 = m * x1 + b;
+var y2 = m * x2 + b;
+
+var slope = ( y2 - y1 ) / ( x2 - x1 );
+
+if ( slope > 0 ) {
+	console.log ("Things are trending up!");
+} else if ( slope < 0 ) {
+	console.log ("things are trending down!");
+} else if ( slope === 0 ) {
+	console.log ("Things are flat!");
+}
